4fae7b438d385cfc4b10e6c230a2b2d312a5f987
tonocastillo <tonocastillo@gmail.com>
2017-08-23 16:40:46 +0000

    new version, checks for 2 different indices and removes indices based on retention days

---
+++ es_retention_enforcer_v2.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import pytz
+import datetime
+import re
+from elasticsearch import Elasticsearch
+import time
+
+
+def delete_index(index):
+    response = None
+    try:
+	print "Deleteting index: %s" % index
+        #response = es.indices.delete(index=index)
+    except:
+        print "Error!"
+    return response
+
+
+if __name__ == '__main__':
+
+    es = Elasticsearch()
+
+    #just for fun, not doint anything, for future reference.
+    #today_date = pytz.utc.localize(datetime.datetime.utcnow())
+    
+
+    # use 8 for a 7 days retention:
+    retention_days = 5
+    retention = pytz.utc.localize(datetime.datetime.utcnow()) - datetime.timedelta(days=retention_days)
+    
+     
+    syslog_indices = es.indices.get(index='syslog-*')
+    netlow_indices = es.indices.get(index='netflow-*')
+
+    for k,v in syslog_indices.iteritems():
+
+        #get creation time of index, this comes in epoch time
+        epoch_time_creation = str(v['settings']['index']['creation_date'])
+	#removing the last 3 digits from epoch string then convert to datetime
+        creation_time_fixed = epoch_time_creation[:-3]
+        #convert epoch to datetime 
+        index_creation_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(float(creation_time_fixed)))
+        index_time_to_datetime = datetime.datetime.strptime(index_creation_time, "%Y-%m-%d %H:%M:%S")
+        #convert from time naive to time aware to be able to perform a time comparation
+        aware_time_conversion = index_time_to_datetime.replace(tzinfo=pytz.UTC)
+
+        if aware_time_conversion < retention:
+#            print k
+            delete_index(k)
+
+    for k,v in netlow_indices.iteritems():
+
+        #get creation time of index, this comes in epoch time
+        epoch_time_creation = str(v['settings']['index']['creation_date'])
+        #removing the last 3 digits from epoch string then convert to datetime
+        creation_time_fixed = epoch_time_creation[:-3]
+        #convert epoch to datetime
+        index_creation_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(float(creation_time_fixed)))
+        index_time_to_datetime = datetime.datetime.strptime(index_creation_time, "%Y-%m-%d %H:%M:%S")
+        #convert from time naive to time aware to be able to perform a time comparation
+        aware_time_conversion = index_time_to_datetime.replace(tzinfo=pytz.UTC)
+
+        if aware_time_conversion < retention:
+#            print k
+            delete_index(k)
+
